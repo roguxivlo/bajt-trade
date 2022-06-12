b02e0998be027dc477ee4e1092c50d368058eef9
Jan Roguwski <Jroguwski@gmail.com>
2022-06-12 13:16:25 +0200

    od nowa

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -1,10 +1,24 @@
 package pl.edu.mimuw;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.io.IOException;
+import pl.edu.mimuw.Gielda.*;
+
 
 public class Main {
 
-  public static void main(String[] args) {
-    final var template = new TemplateClass();
-    final var greeting = template.getGreeting();
-    System.out.println(greeting);
+  public static void main(String[] args) throws IOException {
+    final String json = Files.readString(Paths.get("dane.json"));
+
+    Moshi moshi = new Moshi.Builder().build();
+
+    JsonAdapter<Gielda> jsonAdapter = moshi.adapter(Gielda.class);
+
+    Gielda gielda = jsonAdapter.fromJson(json);
+    System.out.println(gielda.toString());
   }
 }
